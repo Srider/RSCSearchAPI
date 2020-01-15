ef1bd2f710bcc419a7185db09f4bb8f5f65cba6d
Srikar <srikar.ravela@tyfone.com>
2020-01-15 12:42:40 +0530

    Added new version wih fixes

---
+++ 3.0/RSCSearch.framework/Headers/RSCSearch.h
@@ -0,0 +1,19 @@
+//
+//  RSCSearch.h
+//  RSCSearch
+//
+//  Created by Srikar on 07/01/20.
+//  Copyright © 2020 RSC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RSCSearch.
+FOUNDATION_EXPORT double RSCSearchVersionNumber;
+
+//! Project version string for RSCSearch.
+FOUNDATION_EXPORT const unsigned char RSCSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSCSearch/PublicHeader.h>
+
+
